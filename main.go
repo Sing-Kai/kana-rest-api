48297268dfb661e82dd48179e13e60ec2b34a2f6
Sing-Kai Chiu <singkai.chiu@gmail.com>
2021-02-28 15:15:45 +0000

    added audio files and fixed no unique answer bug

--- main.go
+++ main.go
@@ -1,18 +1,28 @@
 package main
 
 import (
+	"log"
 	"net/http"
+	"os"
+	"path/filepath"
+	"strconv"
+	"strings"
 
 	"github.com/Sing-Kai/kana-rest-api.git/quiz"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 
 	r := gin.Default()
+	r.Use(cors.Default())
 	r.GET("/healthy", health)
 	r.GET("/quiz", quiz.HandleKanaQuiz)
-
+	// r.GET("/assets/audio/:id", func(c *gin.Context) {
+	// 	c.File("assets/shi.mp3")
+	// })
+	r.GET("/assets/audio/:id", handleAudioFile)
 	r.Run()
 }
 
@@ -20,4 +30,50 @@ func health(ctx *gin.Context) {
 	ctx.JSON(http.StatusOK, gin.H{
 		"message": "healthy",
 	})
+}
+
+func handleAudioFile(c *gin.Context) {
+	id := c.Param("id")
+	filePath := getAudioFilePath(id)
+	c.File(filePath)
+}
+
+func getAudioFilePath(id string) string {
+
+	i, err := strconv.Atoi(id)
+
+	if err != nil {
+		log.Fatal("error")
+	}
+	//add zere if single digit id
+	if i/10 == 0 {
+		id = "0" + id
+	}
+
+	filePath := search(id)
+
+	return filePath
+}
+
+func search(id string) string {
+
+	fileNames := make([]string, 0)
+	root := "./assets/audio"
+
+	err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
+		fileNames = append(fileNames, path)
+		return nil
+	})
+
+	if err != nil {
+		panic(err)
+	}
+
+	for _, file := range fileNames {
+		if strings.Contains(file, id) {
+			return file
+		}
+	}
+
+	return ""
 }
--- quiz/quiz.go
+++ quiz/quiz.go
@@ -11,8 +11,9 @@ import (
 )
 
 type Quiz struct {
-	Question Kana
-	Answers  []Kana
+	QuestionId int    `json:"questionid"`
+	Question   Kana   `json:"question"`
+	Answers    []Kana `json:"answers"`
 }
 
 type Kana struct {
@@ -23,18 +24,26 @@ type Kana struct {
 }
 
 func HandleKanaQuiz(ctx *gin.Context) {
+
+	totalQuestions := 5
 	numOfAnswers := 5
-	q := getQuiz(numOfAnswers)
-	ctx.JSON(http.StatusOK, q)
+	resp := make([]Quiz, 0)
+	rand.Seed(time.Now().UnixNano())
+
+	for i := 1; i <= totalQuestions; i++ {
+		q := getQuiz(numOfAnswers, i)
+		resp = append(resp, q)
+	}
+
+	ctx.JSON(http.StatusOK, resp)
 }
 
-func getQuiz(ansNum int) Quiz {
+func getQuiz(ansNum int, questionId int) Quiz {
 
 	kanaSlice := readFile()
 	length := len(kanaSlice)
-	rand.Seed(time.Now().UnixNano())
 	question := kanaSlice[rand.Intn(length)]
-	answerIds := getAnswerIds(ansNum, question.Id, length)
+	answerIds := getKanaIds(ansNum, question.Id, length)
 
 	answers := make([]Kana, 0)
 
@@ -46,29 +55,51 @@ func getQuiz(ansNum int) Quiz {
 		}
 	}
 
-	quiz := &Quiz{Question: question, Answers: answers}
+	quiz := &Quiz{QuestionId: questionId, Question: question, Answers: answers}
 
 	return *quiz
 
 }
 
 //gets rand list of character ids including answer
-func getAnswerIds(total, id, length int) []int {
+func getKanaIds(total, questionId, length int) []int {
 
 	ans := make([]int, 0)
-	i := 0
 
-	for i < total-1 {
+	uniqueIds := make(map[int]bool, 0)
+
+	uniqueIds[questionId] = true
+
+	j := 0
+
+	for j < total-1 {
 		//ensure randNum starts with 1
 		randNum := rand.Intn(length-1) + 1
-		if randNum != id {
-			ans = append(ans, randNum)
-			i++
+
+		if _, ok := uniqueIds[randNum]; !ok {
+			uniqueIds[randNum] = true
+			j++
 		}
 	}
 
-	ans = append(ans, id)
-	rand.Seed(time.Now().UnixNano())
+	for k, _ := range uniqueIds {
+		ans = append(ans, k)
+	}
+
+	// i := 0
+
+	// for i < total-1 {
+	// 	//ensure randNum starts with 1
+	// 	randNum := rand.Intn(length-1) + 1
+	// 	if randNum != questionId {
+	// 		ans = append(ans, randNum)
+	// 		i++
+	// 	}
+	// }
+
+	// ans = append(ans, questionId)
+
+	//shuffle answers
 	rand.Shuffle(len(ans), func(i, j int) { ans[i], ans[j] = ans[j], ans[i] })
 
 	return ans
